a4decd08c96f5b04ac43693ef369cfdb5d10796a
Ricky Esparza <RickyEsparza@Rickys-MacBook.local>
2022-08-14 22:20:52 -0600

    added new HTML,JS,CSS files with a bit of code

---
+++ script.js
@@ -0,0 +1,34 @@
+/* Declared variables */
+
+// Variables declared for the introduction page
+var welcome = document.querySelector("#intro");
+var startBtn = document.querySelector("#beginButton");
+var introPage = document.querySelector("#introPage");
+
+// Variables declared for the question page
+var questionPage = document.querySelector("#questionPage");
+var askQuestion = document.querySelector("#questionPrompt");
+
+// Variables declared for the button options
+var reactButtons = document.querySelectorAll("buttonOption");
+var answerBtn1 = document.querySelector("#optionButtonOne");
+var answerBtn2 = document.querySelector("#optionButtonTwo");
+var answerBtn3 = document.querySelector("#optionButtonThree");
+var answerBtn4 = document.querySelector("#optionButtonFour");
+
+// Variables declared for the user answers
+var checkLine = document.querySelector("#bottomLine");
+var scoreBoard = document.querySelector("#submitResults");
+var finalScore = document.querySelector("#userScore");
+var userInitial = document.querySelector("#initials");
+    
+// Variables declared for the end quiz page
+var submitBtn = document.querySelector("#submitButton");
+var highScorePage = document.querySelector("#highScorePage");
+var scoreRecord = document.querySelector("#scoreTracker");
+var scoreCheck = document.querySelector("#scores");
+var finish = document.querySelector("#endQuiz");
+    
+// Variables declared for the high score page
+var backBtn = document.querySelector("#backButton");
+var clearBtn = document.querySelector("#clearButton"); 
