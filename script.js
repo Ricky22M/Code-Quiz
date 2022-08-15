cc7cad13568b9c6487d4fb979a3a88dacf4a6be2
Ricky Esparza <RickyEsparza@Rickys-MacBook.local>
2022-08-14 23:51:45 -0600

    added html comments, added variables to javascript, and added commits to javascript

--- script.js
+++ script.js
@@ -1,34 +1,35 @@
 /* Declared variables */
 
 // Variables declared for the introduction page
-var welcome = document.querySelector("#intro");
-var startBtn = document.querySelector("#beginButton");
+var intro = document.querySelector("#intro");
+var beginButton = document.querySelector("#beginButton");
 var introPage = document.querySelector("#introPage");
 
 // Variables declared for the question page
 var questionPage = document.querySelector("#questionPage");
-var askQuestion = document.querySelector("#questionPrompt");
+var questionPrompt = document.querySelector("#questionPrompt");
 
 // Variables declared for the button options
-var reactButtons = document.querySelectorAll("buttonOption");
-var answerBtn1 = document.querySelector("#optionButtonOne");
-var answerBtn2 = document.querySelector("#optionButtonTwo");
-var answerBtn3 = document.querySelector("#optionButtonThree");
-var answerBtn4 = document.querySelector("#optionButtonFour");
+var answeredButtons = document.querySelectorAll("buttonOption");
+var optionButtonOne = document.querySelector("#optionButtonOne");
+var optionButtonTwo = document.querySelector("#optionButtonTwo");
+var optionButtonThree = document.querySelector("#optionButtonThree");
+var optionButtonFour = document.querySelector("#optionButtonFour");
 
 // Variables declared for the user answers
-var checkLine = document.querySelector("#bottomLine");
+var BottomLine = document.querySelector("#bottomLine");
 var scoreBoard = document.querySelector("#submitResults");
-var finalScore = document.querySelector("#userScore");
+var userScore = document.querySelector("#userScore");
 var userInitial = document.querySelector("#initials");
     
 // Variables declared for the end quiz page
-var submitBtn = document.querySelector("#submitButton");
+var submitButton = document.querySelector("#submitButton");
 var highScorePage = document.querySelector("#highScorePage");
-var scoreRecord = document.querySelector("#scoreTracker");
-var scoreCheck = document.querySelector("#scores");
-var finish = document.querySelector("#endQuiz");
+var scoreTracker = document.querySelector("#scoreTracker");
+var scores = document.querySelector("#scores");
+var endQuiz = document.querySelector("#endQuiz");
     
 // Variables declared for the high score page
-var backBtn = document.querySelector("#backButton");
-var clearBtn = document.querySelector("#clearButton"); 
+var backButtonn = document.querySelector("#backButton");
+var clearButton = document.querySelector("#clearButton"); 
+
